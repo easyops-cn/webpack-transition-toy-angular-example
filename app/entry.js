60e2a423955b75694efb8902ffa9674d6df793e8
J Huang <i@jhuang.me>
2017-05-05 22:56:57 +0800

    refactor(app): import business modules

--- app/entry.js
+++ app/entry.js
@@ -3,3 +3,9 @@ import 'angular'
 import 'angular-route'
 
 import './app'
+import './view1/view1'
+import './view2/view2'
+
+import './components/version/version'
+import './components/version/interpolate-filter'
+import './components/version/version-directive'
